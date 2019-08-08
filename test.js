c818e3f977610d585f515ec95da52f0f7fd14d43
zdczdc10 <53754327+zdczdc10@users.noreply.github.com>
2019-08-08 21:57:59 +0800

    Update test.js

--- test.js
+++ test.js
@@ -13,6 +13,24 @@ function getCookieItem(itemName)
     return usescape(document.cookie.substring(idxStart, idxEnd));
 }
 
+// 设置Cookie
+function setCookie(itemName, itemValue, expireDays, path, domain)
+{
+    var str = itemName + "=" + escape(itemValue);
+    if (expireDays != "") {
+        var now = new Date();
+        now.setTime(now.getTime() + expireDays * 24 * 3600 * 1000);
+        str += ";expires=" + now.toGMTString();
+    }
+    if (path != "") {
+        str += ";path=" + path;
+    }
+    if (domain != "") {
+        str += ";domain=" + domain;
+    }
+    document.cookie = str;
+}
+
 //界面刷新时弹出加载该界面
 window.onload = function() {
 	alert('调用界面成功加载该JS1！');
